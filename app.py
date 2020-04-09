2220a27cd45305a44e2ab3b51b0c7a37b4c1ba5e
grayson <grayson@returntocorp.com>
2020-04-08 17:04:15 -0700

    Add data for random movie quotes. Add index route.

--- app.py
+++ app.py
@@ -1,7 +1,31 @@
-from flask import Flask, request, make_response
+import random
+from flask import Flask, render_template_string, request, make_response, url_for, redirect
 from textwrap import dedent
 
 app = Flask(__name__)
+
+with open("data/quotes.txt", 'r') as fin:
+    quotes = fin.read().split('\n')
+
+def random_quote():
+    return random.choice(quotes).split('|')
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    template = """
+    <html><body>
+    <h1>Random Movie Quotes</h1>
+    <div>
+      <h2>{}</h2>
+      <h4>~{}, {}</h4>
+    </div>
+    </body></html>
+    """
+    try:
+        return render_template_string(template.format(*random_quote()))
+    except Exception:
+        return redirect(url_for("error", url=request.host_url))
+
 
 @app.route("/error", methods=["GET", "POST"])
 def error():
@@ -12,8 +36,9 @@ def error():
     </h1>
     </body></html>
     """
-    resp = make_response(render_template("error.html", request.args.get('url')))
+    resp = make_response(render_template_string(template.format(request.args.get('url'))))
     #resp.headers['Content-Security-Policy'] = "script-src https:"
     return resp
 
-app.run()
+if __name__ == '__main__':
+    app.run()
