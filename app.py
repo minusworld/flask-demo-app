8a653302bf6ff0cdd010d51d9c24ea919c6b3ee9
grayson <grayson@returntocorp.com>
2020-04-09 09:58:55 -0700

    Step2. Refactor app to use template files instead of template strings. Remove render_template string. This removes SSTI. However, this is still susceptible to XSS. (This is final stage of app for demo purposes. This commit illustrates that href template vars should still be detected.)

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import random
-from flask import Flask, render_template_string, request, make_response, url_for, redirect
+from flask import Flask, render_template, request, make_response, url_for, redirect
 from textwrap import dedent
 
 app = Flask(__name__)
@@ -12,32 +12,16 @@ def random_quote():
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    template = """
-    <html><body>
-    <h1>Random Movie Quotes</h1>
-    <div>
-      <h2>{}</h2>
-      <h4>~{}, {}</h4>
-    </div>
-    </body></html>
-    """
     try:
-        return render_template_string(template.format(*random_quote()))
+        quote, movie, year = random_quote()
+        return render_template("index.html", quote=quote, movie=movie, year=year)
     except Exception:
         return redirect(url_for("error", url=request.host_url))
 
 
 @app.route("/error", methods=["GET", "POST"])
 def error():
-    template = """
-    <html><body>
-    <h1>Error: Page not found.
-      <a href="{}">Go back</a>
-    </h1>
-    </body></html>
-    """
-    resp = make_response(render_template_string(template.format(request.args.get('url'))))
-    return resp
+    return render_template("error.html", url=request.args.get('url'))
 
 if __name__ == '__main__':
     app.run()
