4482dc41380f130e195c0b4f2b2c5f5d16001b17
grayson <grayson@returntocorp.com>
2020-04-09 10:13:03 -0700

    Reverts last commit, putting the app back in the starting point for the demo.
    Revert "Step2. Refactor app to use template files instead of template strings. Remove render_template string. This removes SSTI. However, this is still susceptible to XSS. (This is final stage of app for demo purposes. This commit illustrates that href template vars should still be detected.)"
    
    This reverts commit 8a653302bf6ff0cdd010d51d9c24ea919c6b3ee9.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import random
-from flask import Flask, render_template, request, make_response, url_for, redirect
+from flask import Flask, render_template_string, request, make_response, url_for, redirect
 from textwrap import dedent
 
 app = Flask(__name__)
@@ -12,16 +12,32 @@ def random_quote():
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    template = """
+    <html><body>
+    <h1>Random Movie Quotes</h1>
+    <div>
+      <h2>{}</h2>
+      <h4>~{}, {}</h4>
+    </div>
+    </body></html>
+    """
     try:
-        quote, movie, year = random_quote()
-        return render_template("index.html", quote=quote, movie=movie, year=year)
+        return render_template_string(template.format(*random_quote()))
     except Exception:
         return redirect(url_for("error", url=request.host_url))
 
 
 @app.route("/error", methods=["GET", "POST"])
 def error():
-    return render_template("error.html", url=request.args.get('url'))
+    template = """
+    <html><body>
+    <h1>Error: Page not found.
+      <a href="{}">Go back</a>
+    </h1>
+    </body></html>
+    """
+    resp = make_response(render_template_string(template.format(request.args.get('url'))))
+    return resp
 
 if __name__ == '__main__':
     app.run()
